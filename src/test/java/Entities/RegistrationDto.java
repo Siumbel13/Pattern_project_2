17ac4be1fb329bef12b3a641afa25df489122037
Siumbel <sumbelragipova@gmail.com>
2022-11-12 21:57:16 +0300

    initial commit

---
+++ src/test/java/Entities/RegistrationDto.java
@@ -0,0 +1,10 @@
+package Entities;
+
+import lombok.Data;
+
+@Data
+public class RegistrationDto {
+  private final String login;
+  private final String password;
+  private final String status;
+}
